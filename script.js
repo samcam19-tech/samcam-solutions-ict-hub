bb54ed722673598f32e6482baefb4a7219955699
samcam19-tech <samuel.akugizibwe95@gmail.com>
2026-08-21 02:48:37 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1898,3 +1898,13 @@ window.openSubmissionModalWithDetails = function(testId, encodedTitle) {
   }
   testIdEl.value = testId;
 };
+
+function escapeHtml(str) {
+  if (!str) return '';
+  return String(str)
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
+}
