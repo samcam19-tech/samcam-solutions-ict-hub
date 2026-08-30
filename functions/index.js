b4dc3f0b09fd959ae60a48b8cccf5c2f740ccb96
samcam19-tech <samuel.akugizibwe95@gmail.com>
2026-08-30 00:52:15 -0800

    Create index.js

---
+++ functions/index.js
@@ -0,0 +1,31 @@
+const {setGlobalOptions} = require("firebase-functions");
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+setGlobalOptions({maxInstances: 10});
+
+exports.listCollections = onRequest(async (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.set("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    res.status(204).send("");
+    return;
+  }
+
+  try {
+    const collections = await admin.firestore().listCollections();
+    const collectionNames = collections.map((col) => col.id);
+
+    logger.info("Fetched collection names successfully", {collectionNames});
+
+    res.status(200).json({success: true, collections: collectionNames});
+  } catch (error) {
+    logger.error("Error listing collections", error);
+    res.status(500).json({success: false, error: error.message});
+  }
+});
