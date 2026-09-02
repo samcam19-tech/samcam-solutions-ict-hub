04723ba608fda1d431e06be367f17f53448a2923
samcam19-tech <samuel.akugizibwe95@gmail.com>
2026-09-02 06:48:30 -0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2750,11 +2750,11 @@ function toggleMobileMenu() {
 function navigateTo(e, routeKey) {
   if (e) e.preventDefault();
   const urls = {
-  lib: 'e-library/index.html',
+  lib: '../e-library/index.html',
   quiz: '../quiz/',
   portal: '../assessments/'
 };
-  const targetUrl = urls[routeKey] || 'e-library/index.html';
+  const targetUrl = urls[routeKey] || '../index.html';
   window.history.pushState({ route: routeKey }, '', targetUrl);
   window.location.href = targetUrl;
 }
